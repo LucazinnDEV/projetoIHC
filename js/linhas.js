51b5a6a3674fccdf6f35267fab22f713bff78fd4
LarissaGiovanna <lgiova544@gmail.com>
2026-03-30 19:35:26 -0300

    delete linhas.js (nao necessario)

--- js/linhas.js
+++
@@ -1,9 +0,0 @@
-// let galery = document.getElementById("galery");
-// let other_info = document.getElementById("other_info");
-// let top_stops = document.getElementById("top_stops");
-// let terminal = document.getElementById("terminal");
-// let price = document.getElementById("price");
-// let main_infos = document.getElementById("main_infos");
-// let schedules = document.getElementById("schedules");
-// let bus_name = document.getElementById("bus_name");
-
